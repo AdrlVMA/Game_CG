5c616eb7a3925b2162ead4c8d23d0596b86a8b44
adrlvma <adrlvma741@gmail.com>
2021-04-13 23:49:32 -0300

    Adicionando Modificador de pontuação.

---
+++ components/score.js
@@ -0,0 +1,36 @@
+/*AFRAME.registerComponent('score-counter', {
+    schema: {
+      el: {
+        type: 'selector'
+      },
+      score:{
+        type: 'int',
+        default: 0
+      },
+    },
+  
+    init: function () {
+      var sceneEl = document.querySelector('a-scene'); 
+      var scoreBoard = document.querySelector('#score');
+
+      scoreBoard.setAttribute('text', 'value',  newScore)
+  
+      sceneEl.querySelector('a-text').addEventListener(() => {
+        this.data.score++;
+        var newScore = 'Score: ' + this.data.score
+        scoreBoard.setAttribute('text', 'value',  newScore)
+      })
+    }
+});*/
+  
+AFRAME.registerComponent('update-text-every-second', {
+init: function () {
+    var el = this.el;
+    var i = 0;
+
+    setInterval(function () {
+    el.setAttribute('value', "Pontuacao: " + pontuacao, );
+    console.log("--")
+    }, 10);
+}
+});
