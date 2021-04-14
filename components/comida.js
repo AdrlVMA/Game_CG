bbd00d7eed02fee9f48c43d2ebbb07b4170f752e
adrlvma <adrlvma741@gmail.com>
2021-04-13 22:01:39 -0300

    Incrementando pontuação.

--- components/comida.js
+++ components/comida.js
@@ -30,16 +30,18 @@ AFRAME.registerComponent('comida_1', {
         if(posicao_head_x<=this.data.eixo_x_1+range && posicao_head_y<=this.data.eixo_y_1+range && posicao_head_x>=this.data.eixo_x_1-range && posicao_head_y>=this.data.eixo_y_1-range){
             this.el.object3D.position.set(this.data.eixo_x_1, this.data.eixo_y_1, -2);
             console.log("Colidiu");
-            teste_1 = true;
 
             this.data.eixo_x_1 = ((Math.random()*(max_value-(inf_value)) +inf_value));
             this.data.eixo_y_1 = ((Math.random()*(max_value-(inf_value)) +inf_value));
             this.data.eixo_z_1 = 0;
 
             velocidade += incremento;
+            pontuacao += 1;
 
+            teste_1 = true;
+
+        }else{
             teste_1 = false;
-
         }
     }
 });
